c408b39fae5923fb5b56403eed9ab847d8bb9a4e
Michal Hoffman <michal.z.hoffman@gmail.com>
2022-07-14 19:45:04 +0100

    handling del button

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var number1 = '0'
-var number2 = ''
+var number1 = 0
+var number2 = 0
 document.addEventListener('DOMContentLoaded', function (event) {
   document.querySelector('.screen').value = number1
   const main = document.querySelector('.main')
@@ -8,12 +8,17 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
 var clickHandler = (e) => {
   var eText = e.target.innerText
-  var target = e.target
   console.log(eText)
   switch (eText) {
     case 'AC':
-      document.querySelector('.screen').value = 5
-      number1 = '0'
+      document.querySelector('.screen').value = 5444
+      number1 = 5444
       break
+    case 'del':
+      number1 = number1.toString().slice(0, -1)
+      if (number1 === '') {
+        number1 = 0
+      }
+      document.querySelector('.screen').value = number1
   }
 }
